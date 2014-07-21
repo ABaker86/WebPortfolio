df4012afb13afcaec422c2159645476c16fea862
ABaker86 <adambaker@live.com>
2014-07-21 19:24:05 -0400

    Added jQuery class adder to nav and scrtip tags to works page

--- style/scripts/script.js
+++ style/scripts/script.js
@@ -6,3 +6,14 @@ var year = new Date();
 document.getElementById('date').innerHTML = year.getFullYear();
 
 
+var x = document.getElementById("menu-toggle");
+
+x.addEventListener('click', function(){
+	var op = document.getElementById('menu');
+	if(op.className=="simple-toggle "){
+		op.className += 'open';
+	}else
+	{op.className = "simple-toggle ";}
+	
+});
+
